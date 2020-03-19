84053af5e4af7567c2b3a9574b57a5870465fdb1
Jesuraj77 <isysraju2001@gmail.com>
2020-03-19 19:39:29 +0530

    Code Added

---
+++ FilterWebApi.BusinessLayer/FilterServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilterWebApi.BusinessLayer
+{
+   public class FilterServices
+    {
+        // Write code to implement Filtering also Show how to execute filter, paging and sorting together.
+
+
+    }
+}
---
+++ FilterWebApi/Controllers/FilterController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilterWebApi.Controllers
+{
+    public class FilterController : Controller
+    {
+       // Write code to implement Filtering also Show how to execute filter, paging and sorting together.
+
+
+
+    }
+}
